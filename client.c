3da5a58b1865b238d6d215f90eba2ca2a87b8b80
Anna Vasiliou <annidiouss@gmail.com>
2022-11-25 12:11:50 +0200

    update

---
+++ client.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "header.h"
+#include "list.h"
+
+
+int main(int argc, char **argv){
+
+    if(strcmp(argv[2], "-list")== 0){
+
+        
+
+    }
+
+
+    
+
+
+
+
+return 0;
+}
--- list.h
+++ list.h
@@ -4,9 +4,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "header.h"
 
-
-
+void print_list(FILEHEADER * fileheader,INFOHEADER * infoheader);
+int check_file(FILEHEADER * fileheader,INFOHEADER * infoheader);
 
 
 
